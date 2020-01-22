55a422c4109e04c6f1dc3c64555cf4a0a9acab33
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-01-22 15:07:21 +0000

    revise

--- python_fluency2.py
+++ python_fluency2.py
@@ -6,22 +6,19 @@ def only_postivie(lst):
   return [i for i in lst if i > 0]
 
 def add_all(lst):
-  return [sum = 0 i for i in lst sum += i]
+  return sum(lst)
   
 def stripped_strings(lst):
-  return [string for string in arr string = re.sub('\W')]
+  return [re.sub('\W','',string) for string in lst]
   
 def find_special(lst):
   for i in range(len(lst)):
     if lst[i] == 'special':
       return i
     return -1
-  #return [i for i in range(len(lst)) if i == 'special']
   
 def valid_contacts(lstObj):
-  new_lst = []
-  for contact in lstObj:
-    if contact.phoneNumber == 10:
-      new_lst.append(contact)
-  return new_lst
+  return [contact for contact in lstObj if len(contact['phoneNumber']) ==10\\\\\ ]
   
+def names(lst):
+  return [contact['name'] for contact in lst]
